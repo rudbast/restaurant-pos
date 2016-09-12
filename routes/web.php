67e1b1be210fc02881a52918b3291dc1e6f95d37
Rudolf <rudolf_bast@live.com>
2016-09-12 19:08:58 +0700

    Update routes (uses resource keyword)

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,6 @@ Route::get('home', 'HomeController@index');
 
 Route::get('lang/{lang}', 'LanguageController@select');
 
-Route::group(['prefix' => 'users'], function () {
-    Route::get('/', 'UsersController@index');
-    Route::post('/', 'UsersController@store');
-    Route::get('/{user}', 'UsersController@show');
-    Route::put('/{user}/edit', 'UsersController@update');
-    Route::delete('/{user}/delete', 'UsersController@destroy');
-});
+Route::resource('users', 'UsersController', ['except' => [
+    'create', 'edit'
+]]);
